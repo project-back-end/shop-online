f6ed1fc13e646244f12e28747582d1f4c702d615
ngouyuong <31096375+ngouyuong@users.noreply.github.com>
2018-10-18 13:51:53 +0700

    create database

--- database/migrations/2018_10_18_133623_create_save_table.php
+++ database/migrations/2018_10_18_133623_create_save_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateImageTable extends Migration
+class CreateSaveTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,8 @@ class CreateImageTable extends Migration
      */
     public function up()
     {
-        Schema::create('image', function (Blueprint $table) {
+        Schema::create('save', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('image');
-            $table->boolean('status');
-            $table->softDeletes();
             $table->timestamps();
         });
     }
@@ -29,6 +26,6 @@ class CreateImageTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('image');
+        Schema::dropIfExists('save');
     }
 }
